43a2061eee74d50a27a5bdb0404456c7c4cab54a
TheJohnFaraday <jfernandezdinardo@itba.edu.ar>
2021-09-27 20:05:06 -0300

    aceleracion

--- Formulas.h
+++ Formulas.h
@@ -23,3 +23,25 @@ double Incerteza_Angulo(double n, double L, double e, double u_L, double u_e){
 
     return Incerteza;
 }
+
+//l = distancia del recorrido de la particula
+//t = tiempo
+double Incerteza_Aceleracion(double l, double t, double u_l, double u_t){
+    //Derivadas parciales
+    double c1,c2;
+
+    //Con respecto a L
+    c1 = ((2)/(pow(t,2)));
+
+    //Con respecto a t
+    c2 = ((-4*l)/(pow(t,3)));
+
+    //Incerteza Total
+
+    double Incerteza;
+
+    Incerteza = sqrt( pow(c1*u_l, 2) + pow(c2*u_t,2) );
+
+    return Incerteza;
+
+}
